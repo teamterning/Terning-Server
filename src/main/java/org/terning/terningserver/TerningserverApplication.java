9faa40cf4a0b7494a99967b5f304d864d4650b50
Willy <willy_1020@naver.com>
2024-06-25 04:00:59 +0900

    [📂file/#1] 프로젝트 세팅

---
+++ src/main/java/org/terning/terningserver/TerningserverApplication.java
@@ -0,0 +1,13 @@
+package org.terning.terningserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerningserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerningserverApplication.class, args);
+	}
+
+}
