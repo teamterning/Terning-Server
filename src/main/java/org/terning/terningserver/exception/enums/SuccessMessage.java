45b588276f6a3a8d11d5c04fd5c02eb88af3a745
Willy <willy_1020@naver.com>
2024-07-03 13:33:35 +0900

    [⚰️del/#12] 쓸모없는 코드 삭제

--- src/main/java/org/terning/terningserver/exception/enums/SuccessMessage.java
+++ src/main/java/org/terning/terningserver/exception/enums/SuccessMessage.java
@@ -11,7 +11,6 @@ public enum SuccessMessage {
     SUCCESS_GET_CATEGORIES(200, true, "카테고리 리스트 조회를 성공하였습니다."),
     SUCCESS_CREATE_WORD(201, true, "단어를 성공적으로 추가하였습니다."),
     SUCCESS_GET_WORDS(200, true, "단어 리스트 조회를 성공하였습니다."),
-    SUCCESS_GET_MEMORIZED_WORDS(200, true, "외운 단어 리스트 조회를 성공하였습니다."),
     SUCCESS_GET_WORD(200, true, "특정 단어 조회를 성공하였습니다.");
 
     private final int status;
