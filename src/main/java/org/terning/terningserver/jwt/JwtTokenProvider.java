7e54308cd7816cc3f8aa1b0114035376982ad823
권장순 <jsoonworld@gmail.com>
2024-07-06 21:41:29 +0900

    [✨feat/#17]: JWT 토큰 유효성 검사 및 사용자 정보 추출 기능 구현
    - validateToken 메소드를 통해 JWT 토큰의 유효성 검사 로직을 구현.
      - 각 종류의 예외에 따라 적절한 JwtValidationType을 반환.
      - MalformedJwtException, ExpiredJwtException, UnsupportedJwtException, IllegalArgumentException 처리 추가.
    - getUserFromJwt 메소드를 통해 토큰에서 사용자 ID 추출 기능 구현.
    - getBody 메소드를 통해 JWT 파서를 구성하고, 서명 키를 사용하여 토큰의 클레임을 반환하는 로직 구현.
    - 이 변경은 JWT 토큰을 안정적으로 처리하고, 필요한 사용자 정보를 효율적으로 추출하는 기능을 제공합니다.

--- src/main/java/org/terning/terningserver/jwt/JwtTokenProvider.java
+++ src/main/java/org/terning/terningserver/jwt/JwtTokenProvider.java
@@ -1,7 +1,6 @@
 package org.terning.terningserver.jwt;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
@@ -16,6 +15,7 @@ import static io.jsonwebtoken.Header.JWT_TYPE;
 import static io.jsonwebtoken.Header.TYPE;
 import static io.jsonwebtoken.security.Keys.hmacShaKeyFor;
 import static java.util.Base64.getEncoder;
+import static org.terning.terningserver.jwt.JwtValidationType.*;
 
 @Slf4j
 @Component
@@ -34,6 +34,25 @@ public class JwtTokenProvider {
                 .compact();
     }
 
+    public JwtValidationType validateToken(String token) {
+        try {
+            getBody(token);
+            return VALID_JWT;
+        } catch (MalformedJwtException exception) {
+            log.error(exception.getMessage());
+            return INVALID_JWT_TOKEN;
+        } catch (ExpiredJwtException exception) {
+            log.error(exception.getMessage());
+            return EXPIRED_JWT_TOKEN;
+        } catch (UnsupportedJwtException exception) {
+            log.error(exception.getMessage());
+            return UNSUPPORTED_JWT_TOKEN;
+        } catch (IllegalArgumentException exception) {
+            log.error(exception.getMessage());
+            return EMPTY_JWT;
+        }
+    }
+
     private Claims generateClaims(Authentication authentication) {
         val claims = Jwts.claims();
         claims.put("userId", authentication.getPrincipal());
@@ -43,5 +62,18 @@ public class JwtTokenProvider {
     private SecretKey getSigningKey() {
         val encodedKey = getEncoder().encodeToString(valueConfig.getSecretKey().getBytes());
         return hmacShaKeyFor(encodedKey.getBytes());
+    }
+
+    public Long getUserFromJwt(String token) {
+        val claims = getBody(token);
+        return Long.parseLong(claims.get("memberId").toString());
+    }
+
+    private Claims getBody(final String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
