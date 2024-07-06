41df25b5c7775274d0c6de0bdfa1616cb97f7307
권장순 <jsoonworld@gmail.com>
2024-07-06 21:18:30 +0900

    [✨feat/#17]:  JWT 검증 상태 열거형 추가
    - JwtValidationType 열거형을 추가하여 JWT의 유효성 검사 결과를 명확히 구분.
    - VALID_JWT: 유효한 JWT 토큰.
    - INVALID_JWT_TOKEN: 유효하지 않은 형식의 JWT 토큰.
    - EXPIRED_JWT_TOKEN: 만료된 JWT 토큰.
    - UNSUPPORTED_JWT_TOKEN: 지원되지 않는 형식의 JWT 토큰.
    - EMPTY_JWT: 내용이 없는 JWT 토큰.

---
+++ src/main/java/org/terning/terningserver/jwt/JwtValidationType.java
@@ -0,0 +1,9 @@
+package org.terning.terningserver.jwt;
+
+public enum JwtValidationType {
+    VALID_JWT,
+    INVALID_JWT_TOKEN,
+    EXPIRED_JWT_TOKEN,
+    UNSUPPORTED_JWT_TOKEN,
+    EMPTY_JWT
+}
