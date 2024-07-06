6d32dcc80b878f5354387c51dd8bea162ecf5d0d
권장순 <jsoonworld@gmail.com>
2024-07-06 21:11:25 +0900

    [✨feat/#17]: JWT 인증 필터 기본 구조 구현
    - JwtAuthenticationFilter 클래스를 생성하여 OncePerRequestFilter를 상속받음.
    - HttpServletRequest에서 JWT 액세스 토큰을 추출하는 로직 구현.
    - 요청 헤더의 'Authorization'에서 'Bearer ' 접두어를 사용하여 토큰 존재 여부 및 추출 기능 구현.
    - doFilterInternal 메소드는 현재 비어 있으며, 추후 인증 로직을 구현할 예정.

---
+++ src/main/java/org/terning/terningserver/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,46 @@
+package org.terning.terningserver.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.terning.terningserver.config.ValueConfig;
+
+import java.io.IOException;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private static JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+
+    private String getAccessTokenFromRequest(HttpServletRequest request) {
+        return isContainsAccessToken(request) ? getAuthorizationAccessToken(request) : null;
+    }
+
+    private boolean isContainsAccessToken(HttpServletRequest request) {
+        val authorization = request.getHeader(AUTHORIZATION);
+        return authorization != null && authorization.startsWith(ValueConfig.BEARER_HEADER);
+    }
+
+    private String getAuthorizationAccessToken(HttpServletRequest request) {
+        return getTokenFromBearerString(request.getHeader(AUTHORIZATION));
+    }
+
+    private String getTokenFromBearerString(String token) {
+        return token.replaceFirst(ValueConfig.BEARER_HEADER, ValueConfig.BLANK);
+    }
+}
