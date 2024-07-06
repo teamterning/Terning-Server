48d6448bfcf20be8096e3284dc4ba254bae21882
권장순 <jsoonworld@gmail.com>
2024-07-06 23:53:29 +0900

    [✨feat/#17]: UserRepository에 사용자 인증 정보 검색 메소드 추가
    - UserRepository 인터페이스에 findByAuthTypeAndAuthId 메소드 구현.
    - 특정 AuthType과 AuthId를 조합하여 사용자를 검색하는 쿼리 메소드 추가.
    - 반환 타입으로 Optional<User>를 사용하여 결과가 없는 경우를 안전하게 처리.

---
+++ src/main/java/org/terning/terningserver/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package org.terning.terningserver.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.terning.terningserver.domain.User;
+import org.terning.terningserver.domain.enums.AuthType;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByAuthTypeAndAuthId(AuthType authTypeType, String authId);
+}
