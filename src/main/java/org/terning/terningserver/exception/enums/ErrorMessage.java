88ec082041592ff2364970f59e18b69b57be4bf2
권장순 <jsoonworld@gmail.com>
2024-07-06 23:02:18 +0900

    [✨feat/#17]: HTTP 응답 오류 코드 및 메시지 업데이트
    - INVALID_TOKEN, INVALID_KEY 오류 코드 및 메시지 추가.
      - 401 상태 코드와 "유효하지 않은 토큰입니다.", "유효하지 않은 키입니다." 메시지를 정의.

--- src/main/java/org/terning/terningserver/exception/enums/ErrorMessage.java
+++ src/main/java/org/terning/terningserver/exception/enums/ErrorMessage.java
@@ -1,5 +1,7 @@
 package org.terning.terningserver.exception.enums;
 
+
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,7 +10,9 @@ import lombok.Getter;
 public enum ErrorMessage {
 
     NOT_FOUND_INTERN_CATEGORY(404, "해당 인턴 공고는 존재하지 않습니다."),
-    WRONG_PERIOD(404, "해당 단어가 존재하지 않습니다.");
+    WRONG_PERIOD(404, "해당 단어가 존재하지 않습니다."),
+    INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
+    INVALID_KEY(401, "유효하지 않은 키입니다.");
 
     private final int status;
     private final String message;
