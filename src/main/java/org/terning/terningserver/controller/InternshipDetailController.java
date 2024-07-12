06a4af1f1cc2aad843f92792e2604ee5eb959076
정윤 <63058347+JungYoonShin@users.noreply.github.com>
2024-07-12 15:41:16 +0900

    [✨feat/#29] InternshipDetail Swagger 설정
    - 공고 상세페이지, InternshipDetailController에 대한 Swagger 설정

--- src/main/java/org/terning/terningserver/controller/InternshipDetailController.java
+++ src/main/java/org/terning/terningserver/controller/InternshipDetailController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.terning.terningserver.controller.swagger.InternshipDetailSwagger;
 import org.terning.terningserver.dto.internship_detail.InternshipDetailResponse;
 import org.terning.terningserver.exception.dto.SuccessResponse;
 import org.terning.terningserver.service.InternshipDetailService;
@@ -16,12 +17,13 @@ import static org.terning.terningserver.exception.enums.SuccessMessage.SUCCESS_G
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
-public class InternshipDetailController {
+public class InternshipDetailController implements InternshipDetailSwagger {
 
     private final InternshipDetailService internshipDetailService;
 
     @GetMapping("/announcements/{internshipAnnouncementId}")
-    public ResponseEntity<SuccessResponse<InternshipDetailResponse>> getInternshipDetail(@PathVariable("internshipAnnouncementId") Long internshipAnnouncementId) {
+    public ResponseEntity<SuccessResponse<InternshipDetailResponse>> getInternshipDetail(
+            @PathVariable("internshipAnnouncementId") Long internshipAnnouncementId) {
         return ResponseEntity.ok(SuccessResponse.of(
                 SUCCESS_GET_INTERNSHIP_DETAIL,
                 internshipDetailService.getInternshipDetail(internshipAnnouncementId)
