8b17eebeaf8cc1f169464132e6dee2f2b9aa596f
정윤 <63058347+JungYoonShin@users.noreply.github.com>
2024-07-15 03:13:19 +0900

    [✨feat/#31]: 검색 결과화면 Swagger 설정

--- src/main/java/org/terning/terningserver/controller/swagger/SearchSwagger.java
+++ src/main/java/org/terning/terningserver/controller/swagger/SearchSwagger.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.terning.terningserver.dto.search.response.PopularAnnouncementListResponse;
+import org.terning.terningserver.dto.search.response.SearchResultResponse;
 import org.terning.terningserver.exception.dto.SuccessResponse;
 
 @Tag(name = "Search", description = "탐색 관련 API")
@@ -20,6 +21,11 @@ public interface SearchSwagger {
 
     @Operation(summary = "탐색 > 지금 스크랩 수 많은 공고", description = "탐색 화면에서 스크랩 수 많은 공고를 불러오는 API")
     ResponseEntity<SuccessResponse<PopularAnnouncementListResponse>> getMostScrappedAnnouncements(
+
+    );
+
+    @Operation(summary = "탐색 > 검색 결과 화면", description = "탐색 화면에서 인턴 공고를 검색하는 API")
+    ResponseEntity<SuccessResponse<SearchResultResponse>> searchInternshipAnnouncement(
 
     );
 }
