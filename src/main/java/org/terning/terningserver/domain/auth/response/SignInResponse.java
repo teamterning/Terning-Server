1bc9493ea073e3196d6be2eac6685764f86df5b9
권장순 <jsoonworld@gmail.com>
2024-07-15 13:05:15 +0900

    [✨feat/#17]: SignInResponse 레코드에 userId와 authType 필드 추가
    - SignInResponse의 생성자 매개변수에 userId와 AuthType을 추가하여 인증 타입과 사용자 ID를 포함시킴
    - 기존 Token 객체에서 accessToken과 refreshToken을 추출하여 사용하도록 변경
    - 생성자 오버로딩을 통해 기존 Token과 함께 userId 및 authType도 인자로 받도록 수정

--- src/main/java/org/terning/terningserver/domain/auth/response/SignInResponse.java
+++ src/main/java/org/terning/terningserver/domain/auth/response/SignInResponse.java
@@ -2,18 +2,23 @@ package org.terning.terningserver.domain.auth.response;
 
 import lombok.Builder;
 import org.terning.terningserver.domain.Token;
+import org.terning.terningserver.domain.enums.AuthType;
 
 import static lombok.AccessLevel.PRIVATE;
 
 @Builder(access = PRIVATE)
 public record SignInResponse(
         String accessToken,
-        String refreshToken
+        String refreshToken,
+        Long userId,
+        AuthType authType
 ) {
-    public static SignInResponse of(Token token) {
+    public static SignInResponse of(Token token, Long userId, AuthType authType) {
         return SignInResponse.builder()
                 .accessToken(token.getAccessToken())
                 .refreshToken(token.getRefreshToken())
+                .userId(userId)
+                .authType(authType)
                 .build();
     }
 }
