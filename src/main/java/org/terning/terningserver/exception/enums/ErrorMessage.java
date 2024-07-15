6a7f9eaa4bb5b3ac0d7cc514dd61039a32c5e6ff
권장순 <jsoonworld@gmail.com>
2024-07-15 13:08:09 +0900

    [✨feat/#17]: ErrorMessage 열거형에 새로운 에러 메시지 추가
    - 소셜 로그인 관련 에러 메시지 추가 및 업데이트
      - `INVALID_TOKEN`, `INVALID_KEY`, `SOCIAL_LOGIN_FAILED` 등 기존 메시지 정리
      - `TOKEN_REISSUE_FAILED` 메시지 추가로 토큰 재발급 실패 시나리오 처리
    - 회원가입 및 로그인 프로세스 강화:
      - `SIGN_UP_FAILED`, `USER_ALREADY_EXISTS` 에러 메시지 추가로 회원가입 실패 및 중복 가입 관리
    - 사용자 필터링 정보 생성, 로그아웃, 계정 탈퇴 관련 에러 메시지 추가:
      - `SIGN_UP_FILTER_FAILED`, `SIGN_OUT_FAILED`, `REFRESH_TOKEN_RESET_FAILED`, `WITHDRAW_FAILED` 메시지 추가로 각 시나리오별 에러 처리 강화

--- src/main/java/org/terning/terningserver/exception/enums/ErrorMessage.java
+++ src/main/java/org/terning/terningserver/exception/enums/ErrorMessage.java
@@ -7,10 +7,26 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorMessage {
 
+    // 소셜 로그인
     INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
     INVALID_KEY(401, "유효하지 않은 키입니다."),
+    SOCIAL_LOGIN_FAILED(404, "소셜 로그인애 실패하였습니다"),
+    INVALID_USER(404, "유효하지 않은 유저입니다."),
+    TOKEN_REISSUE_FAILED(404, "토큰 재발급에 실패하였습니다."),
 
-    INVALID_USER(404, "유효하지 않은 유저입니다.");
+    // 회원가입
+    SIGN_UP_FAILED(401, "회원가입에 실패하였습니다"),
+    USER_ALREADY_EXISTS(401, "이미 존재하는 유저입니다"),
+
+    // 사용자 필터링 정보 생성
+    SIGN_UP_FILTER_FAILED(404, "회원가입 필터링 정보 생성에 실패하였습니다"),
+
+    // 로그 아웃
+    SIGN_OUT_FAILED(404, "로그아웃에 실패하였습니다"),
+    REFRESH_TOKEN_RESET_FAILED(400, "리프레쉬 토큰 초기화에 실패하였습니다"),
+
+    // 계정 탈퇴
+    WITHDRAW_FAILED(404, "계정 탈퇴에 실패하였습니다");
 
     private final int status;
     private final String message;
