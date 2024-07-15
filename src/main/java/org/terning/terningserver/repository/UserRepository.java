b81b025c915b9b9292041f0a98336d000555e72a
권장순 <jsoonworld@gmail.com>
2024-07-15 13:16:33 +0900

    [♻️refactor/#17]: UserRepository 인터페이스에 새로운 조회 메서드 추가
    - 인증 유형과 액세스 토큰을 사용하여 사용자를 조회하는`findByAuthTypeAndAuthAccessToken` 메서드 추가

--- src/main/java/org/terning/terningserver/repository/UserRepository.java
+++ src/main/java/org/terning/terningserver/repository/UserRepository.java
@@ -8,7 +8,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByAuthTypeAndAuthId(AuthType authTypeType, String authId);
+    Optional<User> findByAuthTypeAndAuthAccessToken(AuthType authType, String authId);
 
     Optional<User> findByRefreshToken(String refreshToken);
+
 }
