4f87a1e4d6a200c663643276a06e9d0d189e0d03
권장순 <jsoonworld@gmail.com>
2024-07-15 13:13:19 +0900

    [✨feat/#17]: JWT 유효성 검사 유형을 위한 enum 추가
    - JwtValidationType enum 추가로 JWT 유효성 검사 상태 명확화
    - `INVALID_JWT_SIGNATURE`: 서명이 유효하지 않은 JWT

--- src/main/java/org/terning/terningserver/jwt/JwtValidationType.java
+++ src/main/java/org/terning/terningserver/jwt/JwtValidationType.java
@@ -2,6 +2,7 @@ package org.terning.terningserver.jwt;
 
 public enum JwtValidationType {
     VALID_JWT,
+    INVALID_JWT_SIGNATURE,
     INVALID_JWT_TOKEN,
     EXPIRED_JWT_TOKEN,
     UNSUPPORTED_JWT_TOKEN,
