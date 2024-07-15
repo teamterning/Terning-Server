930595c243a3289faeb251350fc294771321f7b1
권장순 <jsoonworld@gmail.com>
2024-07-15 13:07:15 +0900

    [♻️refactor/#17]: User 클래스에 필드 및 생성자 업데이트
    - @AllArgsConstructor 어노테이션 추가로 전체 속성 생성자 자동 생성
    - 'filter_id' 필드의 @JoinColumn에서 nullable 제약 조건 제거
    - 'name', 'authId' 필드에 대한 @Column 어노테이션에서 nullable 제약 조건을 제거하고, 문자열 길이 제약만 명시
    - 'authAccessToken' 및 'refreshToken' 필드 추가로 인증 토큰 관리 개선
    - 'updateRefreshToken' 메서드 내 예외 처리 로직 추가

--- src/main/java/org/terning/terningserver/domain/User.java
+++ src/main/java/org/terning/terningserver/domain/User.java
@@ -1,13 +1,13 @@
 package org.terning.terningserver.domain;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.terning.terningserver.domain.common.BaseTimeEntity;
 import org.terning.terningserver.domain.enums.AuthType;
+import org.terning.terningserver.domain.enums.Grade;
 import org.terning.terningserver.domain.enums.State;
+import org.terning.terningserver.domain.enums.WorkingPeriod;
+import org.terning.terningserver.exception.CustomException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,10 +15,13 @@ import java.util.List;
 import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
+import static org.terning.terningserver.exception.enums.ErrorMessage.*;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 @Table(name = "Users")
 public class User extends BaseTimeEntity {
 
@@ -27,44 +30,63 @@ public class User extends BaseTimeEntity {
     private Long id; // 사용자 고유 ID
 
     @OneToOne(fetch = LAZY)
-    @JoinColumn(name="filter_id", nullable = false)
+    @JoinColumn(name="filter_id")
     private Filter filter; // 사용자 필터 설정
     
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Scrap> scrapList = new ArrayList<>(); // 스크랩 공고
 
-    @Column(nullable = false, length = 12)
+    // TODO: 특수문자, 첫글자 , 12자리 이내
+    @Column(length = 12)
     private String name; // 사용자 이름
 
-//    private String email; //이메일
-
-//    private String userImage; //유저 아이콘
+    private Integer profileImage; //유저 아이콘
 
     @Enumerated(STRING)
-    @Column(nullable = false)
     private AuthType authType; // 인증 유형 (예: 카카오, 애플)
 
-    @Column(nullable = false, length = 256)
+    @Column(length = 256)
     private String authId; // 인증 서비스에서 제공하는 고유 ID
 
+    @Column(length = 256)
+    private String authAccessToken; // 엑세스 토큰
+
+    @Column(length = 256)
     private String refreshToken; // 리프레시 토큰
 
+    // TODO: User가 생기면 active default로 바꾸기
     @Enumerated(STRING)
-    @Column(nullable = false)
     private State state; // 사용자 상태 (예: 활성, 비활성, 정지)
 
-    @Builder
-    public User(AuthType authType, String authId) {
-        this.authType = authType;
-        this.authId = authId;
-    }
 
     public void updateRefreshToken(String refreshToken) {
         this.refreshToken = refreshToken;
     }
 
     public void resetRefreshToken() {
-        this.refreshToken = null;
+        try {
+            this.refreshToken = null;
+        } catch (Exception e) {
+            throw new CustomException(REFRESH_TOKEN_RESET_FAILED);
+        }
     }
 
+    public void updateProfile(String name, Integer profileImage) {
+        if (name != null && !name.isEmpty()) {
+            this.name = name;
+        }
+        if (profileImage != null) {
+            this.profileImage = profileImage;
+        }
+    }
+
+    @Builder
+    public void updateFilter(Grade grade, WorkingPeriod workingPeriod, int startYear, int startMonth) {
+        this.filter = Filter.builder()
+                .grade(grade)
+                .workingPeriod(workingPeriod)
+                .startYear(startYear)
+                .startMonth(startMonth)
+                .build();
+    }
 }
