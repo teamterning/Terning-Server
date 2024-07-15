ced8055fcb9694fa6691c6edf046e86d183813c2
권장순 <jsoonworld@gmail.com>
2024-07-15 13:27:24 +0900

    [✨feat/#17]: FilterRepository 인터페이스 추가
    - Filter 엔티티에 대한 JpaRepository 인터페이스 `FilterRepository` 추가
    - 이 인터페이스는 Filter 객체에 대한 데이터 접근을 추상화하여, CRUD 기능을 자동으로 제공받을 수 있게 함
    - Spring Data JPA의 Repository 어노테이션을 사용하여 스프링 컨테이너에 등록

---
+++ src/main/java/org/terning/terningserver/repository/FilterRepository.java
@@ -0,0 +1,9 @@
+package org.terning.terningserver.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.terning.terningserver.domain.Filter;
+
+@Repository
+public interface FilterRepository extends JpaRepository<Filter, Long> {
+}
