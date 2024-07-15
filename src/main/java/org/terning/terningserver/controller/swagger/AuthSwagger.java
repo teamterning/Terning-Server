b00ce313fadbc5fc50cca465bb0fab93b8635f55
권장순 <jsoonworld@gmail.com>
2024-07-15 18:30:56 +0900

    [✨feat/#17]: Swagger API 문서화를 위한 인터페이스 추가
    - AuthSwagger 인터페이스를 추가하여 소셜 로그인, 회원가입, 토큰 재발급, 사용자 필터링 정보 생성, 로그아웃, 계정 탈퇴에 대한 API 설명을 명시적으로 정의하였습니다.
    - 각 API 메서드에 `@Operation` 어노테이션을 사용하여 요약과 세부 설명을 제공함으로써, API 사용자가 각 엔드포인트의 용도와 기능을 쉽게 이해할 수 있도록 하였습니다.
    - `@Tag` 어노테이션을 사용하여 이 인터페이스가 소셜 로그인 및 회원가입과 관련된 API를 다루는 것임을 명확히 하였습니다.

---
+++ src/main/java/org/terning/terningserver/controller/swagger/AuthSwagger.java
@@ -0,0 +1,46 @@
+package org.terning.terningserver.controller.swagger;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.terning.terningserver.domain.auth.response.SignInResponse;
+import org.terning.terningserver.domain.auth.response.SignUpFilterResponse;
+import org.terning.terningserver.domain.auth.response.TokenGetResponse;
+import org.terning.terningserver.domain.enums.AuthType;
+import org.terning.terningserver.exception.dto.SuccessResponse;
+
+@Tag(name = "Auth", description = "소셜 로그인 및 회원가입 API")
+public interface AuthSwagger {
+
+    @Operation(summary = "소셜 로그인", description = "AuthType에 맞는 소셜 로그인 API")
+    ResponseEntity<SuccessResponse<SignInResponse>> signIn(
+            String accessToken,
+            String refreshToken,
+            Long userId,
+            AuthType authType
+    );
+
+    @Operation(summary = "토큰 재발급", description = "토큰 재발급 API")
+    ResponseEntity<SuccessResponse<TokenGetResponse>> reissueToken(
+            String accessToken
+    );
+
+    @Operation(summary = "사용자 필터링 정보 생성", description = "사용자 필터링 정보 생성 API")
+    ResponseEntity<SuccessResponse<SignUpFilterResponse>> filter(
+
+    );
+
+    @Operation(summary = "회원가입", description = "회원가입 API")
+    ResponseEntity<SuccessResponse<SignInResponse>> signUp(
+    );
+
+    @Operation(summary = "로그아웃", description = "로그아웃 API")
+    ResponseEntity<SuccessResponse> signOut(
+    );
+
+    @Operation(summary = "계정탈퇴", description = "계정탈퇴 API")
+    ResponseEntity<SuccessResponse> withdraw(
+    );
+
+
+}
