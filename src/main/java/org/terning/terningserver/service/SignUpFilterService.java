f4ce4f8bfc08c4f0f99f5341d02ba08f1961c2fd
권장순 <jsoonworld@gmail.com>
2024-07-15 13:25:53 +0900

    [✨feat:/#17]: SignUpFilterService에 회원 필터 정보 등록 기능 추가
    - `signUpFilter` 메서드 구현으로 사용자 필터 정보 생성 및 업데이트 기능 제공
    - 입력받은 회원 ID를 사용하여 해당 사용자를 찾고, 없을 경우 `CustomException`을 발생시키며 `SIGN_UP_FILTER_FAILED` 에러 메시지 반환
    - `SignUpFilterRequest`에서 제공된 grade, workingPeriod, startYear, startMonth를 사용하여 `Filter` 객체 생성
    - 생성된 `Filter` 객체를 `filterRepository`를 통해 저장, 실패 시 `CustomException` 발생
    - 사용자 모델에 필터 정보를 업데이트 후 `userRepository`를 통해 저장

---
+++ src/main/java/org/terning/terningserver/service/SignUpFilterService.java
@@ -0,0 +1,52 @@
+package org.terning.terningserver.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.terning.terningserver.domain.Filter;
+import org.terning.terningserver.domain.User;
+import org.terning.terningserver.domain.auth.request.SignUpFilterRequest;
+import org.terning.terningserver.domain.enums.Grade;
+import org.terning.terningserver.domain.enums.WorkingPeriod;
+import org.terning.terningserver.exception.CustomException;
+import org.terning.terningserver.repository.FilterRepository;
+import org.terning.terningserver.repository.UserRepository;
+
+import static org.terning.terningserver.exception.enums.ErrorMessage.SIGN_UP_FILTER_FAILED;
+
+@Service
+@RequiredArgsConstructor
+public class SignUpFilterService {
+
+    private final FilterRepository filterRepository;
+    private final UserRepository userRepository;
+
+    public void signUpFilter(Long userId, SignUpFilterRequest request) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new CustomException(SIGN_UP_FILTER_FAILED));
+
+        int gradeKey = request.grade();
+        int workingPeriodKey = request.workingPeriod();
+        int startYear = request.startYear();
+        int startMonth = request.startMonth();
+
+        Grade grade = Grade.fromKey(gradeKey);
+        WorkingPeriod workingPeriod = WorkingPeriod.fromKey(workingPeriodKey);
+
+        Filter filter = Filter.builder()
+                .grade(grade)
+                .workingPeriod(workingPeriod)
+                .startYear(startYear)
+                .startMonth(startMonth)
+                .build();
+
+        try {
+            filterRepository.save(filter);
+        } catch (Exception e) {
+            throw new CustomException(SIGN_UP_FILTER_FAILED);
+        }
+
+
+        user.updateFilter(grade, workingPeriod, startYear, startMonth);
+        userRepository.save(user);
+    }
+
+}
