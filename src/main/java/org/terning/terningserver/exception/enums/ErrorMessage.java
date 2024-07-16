aec75c5343c1693faab5b6a5278a44dc49a5368f
권장순 <jsoonworld@gmail.com>
2024-07-16 16:43:36 +0900

    [♻️refactor/#17]: ErrorMessage 열거형 코드 정리 및 메시지 수정
    - 중복된 에러 메시지 및 코드 정리
    - SOCIAL_LOGIN_FAILED, TOKEN_REISSUE_FAILED, SIGN_UP_FAILED, USER_ALREADY_EXISTS 등 중복된 메시지 제거

--- src/main/java/org/terning/terningserver/exception/enums/ErrorMessage.java
+++ src/main/java/org/terning/terningserver/exception/enums/ErrorMessage.java
@@ -10,23 +10,23 @@ public enum ErrorMessage {
     // 소셜 로그인
     INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
     INVALID_KEY(401, "유효하지 않은 키입니다."),
-    SOCIAL_LOGIN_FAILED(404, "소셜 로그인애 실패하였습니다"),
+    FAILED_SOCIAL_LOGIN(404, "소셜 로그인애 실패하였습니다"),
     INVALID_USER(404, "유효하지 않은 유저입니다."),
-    TOKEN_REISSUE_FAILED(404, "토큰 재발급에 실패하였습니다."),
+    FAILED_TOKEN_REISSUE(404, "토큰 재발급에 실패하였습니다."),
 
     // 회원가입
-    SIGN_UP_FAILED(401, "회원가입에 실패하였습니다"),
-    USER_ALREADY_EXISTS(401, "이미 존재하는 유저입니다"),
+    FAILED_SIGN_UP(401, "회원가입에 실패하였습니다"),
+    EXISTS_USER_ALREADY(401, "이미 존재하는 유저입니다"),
 
     // 사용자 필터링 정보 생성
-    SIGN_UP_FILTER_FAILED(404, "회원가입 필터링 정보 생성에 실패하였습니다"),
+    FAILED_SIGN_UP_FILTER(404, "회원가입 필터링 정보 생성에 실패하였습니다"),
 
     // 로그 아웃
-    SIGN_OUT_FAILED(404, "로그아웃에 실패하였습니다"),
-    REFRESH_TOKEN_RESET_FAILED(400, "리프레쉬 토큰 초기화에 실패하였습니다"),
+    FAILED_SIGN_OUT(404, "로그아웃에 실패하였습니다"),
+    FAILED_REFRESH_TOKEN_RESET(400, "리프레쉬 토큰 초기화에 실패하였습니다"),
 
     // 계정 탈퇴
-    WITHDRAW_FAILED(404, "계정 탈퇴에 실패하였습니다"),
+    FAILED_WITHDRAW(404, "계정 탈퇴에 실패하였습니다"),
     
     //404(NotFound)
     NOT_FOUND_INTERN_CATEGORY(404, "해당 인턴 공고는 존재하지 않습니다"),
