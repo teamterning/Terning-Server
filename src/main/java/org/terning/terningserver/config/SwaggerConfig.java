d217dec973882747946da9035b0ab96f8b49fdce
권장순 <jsoonworld@gmail.com>
2024-07-17 17:28:23 +0900

    [🔥!hotfix/#develop] CORS 에러 해결

--- src/main/java/org/terning/terningserver/config/SwaggerConfig.java
+++ src/main/java/org/terning/terningserver/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package org.terning.terningserver.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -9,6 +11,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "/", description = "Default Server url")
+        }
+)
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
