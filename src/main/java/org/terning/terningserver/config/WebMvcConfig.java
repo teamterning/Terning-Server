93b7372a8c3b3534c2761619aec3ace86cb0756a
Willy <willy_1020@naver.com>
2024-07-17 16:04:50 +0900

    [🔥!hotfix/#63] CORS 에러 해결

--- src/main/java/org/terning/terningserver/config/WebMvcConfig.java
+++ src/main/java/org/terning/terningserver/config/WebMvcConfig.java
@@ -2,16 +2,14 @@ package org.terning.terningserver.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*") // 허용할 출처 : 특정 도메인만 받을 수 있음
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "http://terning-official.p-e.kr") // 허용할 출처 : 특정 도메인만 받을 수 있음
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // 허용할 HTTP method
                 .allowCredentials(true); // 쿠키 인증 요청 허용
     }
