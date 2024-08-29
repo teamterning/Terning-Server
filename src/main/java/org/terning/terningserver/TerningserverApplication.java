660d30be6d814144017200a24b69e0423af22cf2
Willy <willy_1020@naver.com>
2024-08-30 00:46:04 +0900

    [test] CI 테스트

--- src/main/java/org/terning/terningserver/TerningserverApplication.java
+++ src/main/java/org/terning/terningserver/TerningserverApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class TerningserverApplication {
 
-	public static void main(String[] args) { //run
+	public static void main(String[] args) {
 		SpringApplication.run(TerningserverApplication.class, args);
 	}
 
