2acd0ba6f112f4cf198e4fa6c76a06457b4fff43
Willy <willy_1020@naver.com>
2024-08-30 11:02:31 +0900

    [✨feat] 홈 > 오늘 마감되는 관심공고에서 홈 > 곧 마감되는 관심공고로 변경(HomeController)

--- src/main/java/org/terning/terningserver/controller/HomeController.java
+++ src/main/java/org/terning/terningserver/controller/HomeController.java
@@ -6,19 +6,15 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.terning.terningserver.controller.swagger.HomeSwagger;
 import org.terning.terningserver.dto.user.response.HomeAnnouncementsResponseDto;
-import org.terning.terningserver.dto.user.response.HomeResponseDto;
-import org.terning.terningserver.dto.user.response.TodayScrapResponseDto;
-import org.terning.terningserver.exception.CustomException;
-import org.terning.terningserver.exception.dto.ErrorResponse;
+import org.terning.terningserver.dto.user.response.UpcomingScrapResponseDto;
 import org.terning.terningserver.exception.dto.SuccessResponse;
-import org.terning.terningserver.exception.enums.SuccessMessage;
 import org.terning.terningserver.service.HomeService;
 import org.terning.terningserver.service.ScrapService;
 
 import java.util.List;
 
 import static org.terning.terningserver.exception.enums.SuccessMessage.SUCCESS_GET_ANNOUNCEMENTS;
-import static org.terning.terningserver.exception.enums.SuccessMessage.SUCCESS_GET_TODAY_ANNOUNCEMENTS;
+import static org.terning.terningserver.exception.enums.SuccessMessage.SUCCESS_GET_UPCOMING_ANNOUNCEMENTS;
 
 @RestController
 @RequiredArgsConstructor
@@ -40,13 +36,13 @@ public class HomeController implements HomeSwagger {
         return ResponseEntity.ok(SuccessResponse.of(SUCCESS_GET_ANNOUNCEMENTS, announcements));
     }
 
-    @GetMapping("/home/today")
-    public ResponseEntity<SuccessResponse<List<TodayScrapResponseDto>>> getTodayScraps(
+    @GetMapping("/home/upcoming")
+    public ResponseEntity<SuccessResponse<List<UpcomingScrapResponseDto>>> getUpcomingScraps(
             @AuthenticationPrincipal Long userId
     ){
 
-        List<TodayScrapResponseDto> scrapList = scrapService.getTodayScrap(userId);
+        List<UpcomingScrapResponseDto> scrapList = scrapService.getUpcomingScrap(userId);
 
-        return ResponseEntity.ok(SuccessResponse.of(SUCCESS_GET_TODAY_ANNOUNCEMENTS, scrapList));
+        return ResponseEntity.ok(SuccessResponse.of(SUCCESS_GET_UPCOMING_ANNOUNCEMENTS, scrapList));
     }
 }
