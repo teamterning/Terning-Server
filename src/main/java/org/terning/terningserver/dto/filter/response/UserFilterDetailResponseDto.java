19faa8b3f2f3e632e07cf722a64c3707c5c77269
정윤 <63058347+JungYoonShin@users.noreply.github.com>
2024-09-07 20:41:23 +0900

    [♻️refactor/#127]: UserFilterDetailResponseDto @Builder 접근제어자 private으로 설정

--- src/main/java/org/terning/terningserver/dto/filter/response/UserFilterDetailResponseDto.java
+++ src/main/java/org/terning/terningserver/dto/filter/response/UserFilterDetailResponseDto.java
@@ -3,14 +3,16 @@ package org.terning.terningserver.dto.filter.response;
 import lombok.Builder;
 import org.terning.terningserver.domain.Filter;
 
-@Builder
+import static lombok.AccessLevel.PRIVATE;
+
+@Builder(access = PRIVATE)
 public record UserFilterDetailResponseDto(
         String grade,
         String workingPeriod,
         Integer startYear,
         Integer startMonth
 ) {
-    public static UserFilterDetailResponseDto of(Filter userFilter) {
+    public static UserFilterDetailResponseDto of(final Filter userFilter) {
         return UserFilterDetailResponseDto.builder()
                 .grade(userFilter == null ? null : userFilter.getGrade().getKey())
                 .workingPeriod(userFilter == null ? null : userFilter.getWorkingPeriod().getKey())
