dfd4eb2555b5b8cf9d82b7d67691a20907bb3003
Willy <willy_1020@naver.com>
2024-09-27 22:46:16 +0900

    [deploy] 배포 테스트

--- src/main/java/org/terning/terningserver/TerningserverApplication.java
+++ src/main/java/org/terning/terningserver/TerningserverApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class TerningserverApplication {
 
+	//terning.app
 	public static void main(String[] args) {
 		SpringApplication.run(TerningserverApplication.class, args);
 	}
