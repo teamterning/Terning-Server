2fbcbd28974c92018b02da810bcc9a53e3ef5c94
Willy <willy_1020@naver.com>
2025-01-19 02:41:11 +0900

    [🔥 !hotfix] 스테이징 서버 마이그레이션 작업 진행

--- src/main/java/org/terning/terningserver/config/WebMvcConfig.java
+++ src/main/java/org/terning/terningserver/config/WebMvcConfig.java
@@ -16,7 +16,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
                         "http://localhost:3000",
                         "https://www.terning-official.p-e.kr/",
                         "https://www.terning-official.n-e.kr/",
-                        "http://15.165.242.132") // 허용할 출처 : 특정 도메인만 받을 수 있음
+                        "http://15.165.242.132",
+                        "http://54.180.215.35") // 허용할 출처 : 특정 도메인만 받을 수 있음
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // 허용할 HTTP method
                 .allowCredentials(true); // 쿠키 인증 요청 허용
     }
