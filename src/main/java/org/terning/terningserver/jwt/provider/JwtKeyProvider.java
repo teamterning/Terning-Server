efb51a34e5e34f4adaca7152aa6c3cd4496ab2c3
권장순 <jsoonworld@gmail.com>
2025-03-09 23:22:43 +0900

    feat(JwtKeyProvider) : JWT 서명 키 제공 기능 구현
    - JwtKeyProvider 클래스를 추가하여 JWT 서명을 위한 SecretKey를 제공하는 기능을 구현하였습니다.
    - ValueConfig에서 비밀 키를 가져와 HMAC SHA 알고리즘을 사용하여 SecretKey를 생성하도록 처리하였습니다.

---
+++ src/main/java/org/terning/terningserver/jwt/provider/JwtKeyProvider.java
@@ -0,0 +1,14 @@
+package org.terning.terningserver.jwt.provider;
+
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import org.terning.terningserver.config.ValueConfig;
+
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtKeyProvider {
+    public static SecretKey getSigningKey(ValueConfig valueConfig) {
+        return Keys.hmacShaKeyFor(valueConfig.getSecretKey().getBytes());
+    }
+}
