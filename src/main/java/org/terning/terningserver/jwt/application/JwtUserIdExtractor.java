0a0bcd6f52e158fc288f9ad2f04760ccabee05d4
권장순 <jsoonworld@gmail.com>
2025-03-09 23:21:16 +0900

    feat(JwtUserIdExtractor) : JWT에서 사용자 ID 추출 기능 구현
    - JwtUserIdExtractor 클래스를 추가하여 JWT 토큰에서 사용자 ID를 추출하는 기능을 구현하였습니다.
    - Claims에서 "userId" 값을 가져오고, UserIdConverter를 통해 Long 타입으로 변환하도록 처리하였습니다.
    - 유효하지 않은 타입일 경우 JwtException을 던지도록 예외 처리를 추가하였습니다.

---
+++ src/main/java/org/terning/terningserver/jwt/application/JwtUserIdExtractor.java
@@ -0,0 +1,24 @@
+package org.terning.terningserver.jwt.application;
+
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.terning.terningserver.jwt.auth.UserIdConverter;
+import org.terning.terningserver.jwt.exception.JwtErrorCode;
+import org.terning.terningserver.jwt.exception.JwtException;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class JwtUserIdExtractor {
+    private static final String CLAIM_USER_ID = "userId";
+    private final JwtClaimsParser jwtClaimsParser;
+
+    public Long extractUserId(String token) {
+        Claims claims = jwtClaimsParser.parse(token);
+        return Optional.ofNullable(claims.get(CLAIM_USER_ID))
+                .map(UserIdConverter::convertToLong)
+                .orElseThrow(() -> new JwtException(JwtErrorCode.INVALID_USER_ID_TYPE));
+    }
+}
