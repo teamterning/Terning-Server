4bf1e632611bc1758f2eeefd184365c7834a120a
권장순 <jsoonworld@gmail.com>
2025-03-09 22:44:57 +0900

    feat(JwtErrorCode) : JWT 예외 코드 관리 enum 구현
    - JWT 관련 예외 코드를 `JwtErrorCode` enum으로 정의하여 관리
    - `INVALID_JWT_TOKEN`, `INVALID_USER_ID`, `INVALID_USER_ID_TYPE` 예외 코드 추가
    - 예외 메시지에 `[AUTH ERROR]` 접두사를 추가하여 오류 유형을 명확하게 구분

---
+++ src/main/java/org/terning/terningserver/jwt/exception/JwtErrorCode.java
@@ -0,0 +1,22 @@
+package org.terning.terningserver.jwt.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum JwtErrorCode {
+    INVALID_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 JWT 토큰입니다."),
+    INVALID_USER_ID(HttpStatus.BAD_REQUEST, "유효하지 않은 userId 값입니다."),
+    INVALID_USER_ID_TYPE(HttpStatus.BAD_REQUEST, "유효하지 않은 userId 타입입니다.");
+
+    public static final String PREFIX = "[AUTH ERROR]";
+
+    private final HttpStatus status;
+    private final String rawMessage;
+
+    public String getMessage() {
+        return PREFIX + " " + rawMessage;
+    }
+}
