ad2101531b8b1a897a31af5c2b27ed095b15a5e0
권장순 <jsoonworld@gmail.com>
2025-03-18 20:29:04 +0900

    feat(GlobalExceptionHandler) : 인증 예외 처리 로직 추가
    - `AuthException`을 처리하는 예외 핸들러 추가
    - `AuthErrorCode`를 기반으로 HTTP 응답 상태 및 에러 메시지 반환
    - 기존 `JwtException` 처리 방식과 일관성을 유지하여 예외 관리 구조 개선

--- src/main/java/org/terning/terningserver/exception/GlobalExceptionHandler.java
+++ src/main/java/org/terning/terningserver/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package org.terning.terningserver.exception;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.terning.terningserver.auth.common.exception.AuthErrorCode;
+import org.terning.terningserver.auth.common.exception.AuthException;
 import org.terning.terningserver.jwt.exception.JwtErrorCode;
 import org.terning.terningserver.jwt.exception.JwtException;
 import org.terning.terningserver.exception.dto.ErrorResponse;
@@ -23,6 +25,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(JwtException.class)
     public ResponseEntity<ErrorResponse> handleAuthException(JwtException e) {
         JwtErrorCode errorCode = e.getErrorCode();
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(ErrorResponse.of(errorCode.getStatus().value(), errorCode.getMessage()));
+    }
+
+    @ExceptionHandler(AuthException.class)
+    public ResponseEntity<ErrorResponse> handleAuthException(AuthException e) {
+        AuthErrorCode errorCode = e.getErrorCode();
         return ResponseEntity
                 .status(errorCode.getStatus())
                 .body(ErrorResponse.of(errorCode.getStatus().value(), errorCode.getMessage()));
