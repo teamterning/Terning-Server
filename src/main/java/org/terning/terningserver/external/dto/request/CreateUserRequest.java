bf7a5281127ac301eda1e89e542c68c85f3266b3
Willy <willy_1020@naver.com>
2025-03-31 23:56:09 +0900

    [✨ feat] 알림서버로 보내는 응답값 수정

--- src/main/java/org/terning/terningserver/external/dto/request/CreateUserRequest.java
+++ src/main/java/org/terning/terningserver/external/dto/request/CreateUserRequest.java
@@ -1,8 +1,10 @@
 package org.terning.terningserver.external.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.terning.terningserver.domain.enums.AuthType;
 
 public record CreateUserRequest(
+        @JsonProperty("oUserId")
         Long userId,
         String name,
         AuthType authType,
