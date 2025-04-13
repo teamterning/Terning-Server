d688ebba94c4ff047f223ad2d279d9572a02ba35
Willy <willy_1020@naver.com>
2025-04-14 01:38:17 +0900

    [chore] RS256 알고리즘 재적용

--- src/main/java/org/terning/terningserver/jwt/provider/JwtKeyProvider.java
+++ src/main/java/org/terning/terningserver/jwt/provider/JwtKeyProvider.java
@@ -1,7 +1,7 @@
 package org.terning.terningserver.jwt.provider;
 
 import io.jsonwebtoken.security.Keys;
-import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import org.springframework.stereotype.Component;
 import org.terning.terningserver.config.ValueConfig;
 
@@ -10,6 +10,10 @@ import javax.crypto.SecretKey;
 @Component
 public class JwtKeyProvider {
     public static SecretKey getSigningKey(ValueConfig valueConfig) {
-        return Keys.hmacShaKeyFor(valueConfig.getSecretKey().getBytes(StandardCharsets.UTF_8));
+//        return Keys.hmacShaKeyFor(valueConfig.getSecretKey().getBytes(StandardCharsets.UTF_8));
+        byte[] keyBytes = Base64.getDecoder().decode(valueConfig.getSecretKey());
+        SecretKey key = Keys.hmacShaKeyFor(keyBytes);
+        System.out.println("Using secret key with bit length: " + (key.getEncoded().length * 8) + " bits");
+        return key;
     }
 }
