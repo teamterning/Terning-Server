af2c537a2432439b02d18ac8b0a3d4b9687c6ae3
Willy <willy_1020@naver.com>
2025-04-15 05:57:24 +0900

    [✨ feat] 소셜로그인 시, 클라이언트 측에서 fcmToken을 RequestBody로 전달하도록 추가

--- src/main/java/org/terning/terningserver/auth/dto/request/SignInRequest.java
+++ src/main/java/org/terning/terningserver/auth/dto/request/SignInRequest.java
@@ -3,8 +3,8 @@ package org.terning.terningserver.auth.dto.request;
 import lombok.NonNull;
 import org.terning.terningserver.domain.enums.AuthType;
 
-public record SignInRequest(@NonNull AuthType authType) {
-    public static SignInRequest of(AuthType authType){
-        return new SignInRequest(authType);
+public record SignInRequest(@NonNull AuthType authType, String fcmToken) {
+    public static SignInRequest of(AuthType authType, String fcmToken){
+        return new SignInRequest(authType, fcmToken);
     }
 }
