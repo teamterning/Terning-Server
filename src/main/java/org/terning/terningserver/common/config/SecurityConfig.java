e17f94d4599884638019fcbecc0f5c92b66ebb2c
Willy <willy_1020@naver.com>
2025-05-11 20:08:06 +0900

    [CHORE] prePostEnabled를 적용하기 위한 Config 어노테이션 추가

--- src/main/java/org/terning/terningserver/common/config/SecurityConfig.java
+++ src/main/java/org/terning/terningserver/common/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.terning.terningserver.common.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,6 +16,7 @@ import org.terning.terningserver.common.security.jwt.filter.JwtAuthenticationFil
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity(pre)
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
